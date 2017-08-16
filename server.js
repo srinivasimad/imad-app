ce801166325f7c445eaeea99f97185dee6a6f13b
srinivasimad <30686772+srinivasimad@users.noreply.github.com>
2017-08-16 12:56:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,20 +6,52 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
-    title: 'Article One | Srinivas Gundoju',
-    heading: 'Article One',
-    date: '8th August, 2017',
-    content: `
-        <p>
-            This is the content of my first article This is the content of my first article This is the content of my first article This is the content of my first article 
-        </p>
-        <p>
-            This is the content of my first article This is the content of my first article This is the content of my first article This is the content of my first article 
-        </p>
-        <p>
-            This is the content of my first article This is the content of my first article This is the content of my first article This is the content of my first article 
-        </p>`
+var articles = {
+    'article-one': {
+        title: 'Article One | Srinivas Gundoju',
+        heading: 'Article One',
+        date: '8th August, 2017',
+        content: `
+            <p>
+                This is the content of my first article This is the content of my first article This is the content of my first article This is the content of my first article 
+            </p>
+            <p>
+                This is the content of my first article This is the content of my first article This is the content of my first article This is the content of my first article 
+            </p>
+            <p>
+                This is the content of my first article This is the content of my first article This is the content of my first article This is the content of my first article 
+            </p>`
+    },
+    'article-two': {
+        title: 'Article Two | Srinivas Gundoju',
+        heading: 'Article Two',
+        date: '8th August, 2017',
+        content: `
+            <p>
+                This is the content of my second article This is the content of my second article This is the content of my second article This is the content of my second article 
+            </p>
+            <p>
+                This is the content of my second article This is the content of my second article This is the content of my second article This is the content of my second article 
+            </p>
+            <p>
+                This is the content of my second article This is the content of my second article This is the content of my second article This is the content of my second article 
+            </p>`
+    },
+    'article-three': {
+        title: 'Article Three | Srinivas Gundoju',
+        heading: 'Article Three',
+        date: '8th August, 2017',
+        content: `
+            <p>
+                This is the content of my third article This is the content of my third article This is the content of my third article This is the content of my third article 
+            </p>
+            <p>
+                This is the content of my third article This is the content of my third article This is the content of my third article This is the content of my third article 
+            </p>
+            <p>
+                This is the content of my third article This is the content of my third article This is the content of my third article This is the content of my third article 
+            </p>`
+    },
 };
 
 function createTemplate (data) {
@@ -62,16 +94,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res) {
-    res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function(req, res) {
+    //articleName == article-one
+    //aricles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
